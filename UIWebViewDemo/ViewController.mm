75fba4a7fbd8f439b3ab8b93878148779fbfd510
wangqianzhou <wangqianzhou@foxmail.com>
2017-01-23 18:00:57 +0800

    commit branch project

--- UIWebViewDemo/ViewController.mm
+++ UIWebViewDemo/ViewController.mm
@@ -12,7 +12,12 @@
 #import "CustomButton.h"
 #import "UIView+Addtions.h"
 #import "URLViewController.h"
-
+#include <sys/sysctl.h>
+#include <mach/mach.h>
+#include <mach/mach_init.h>
+#include <mach/task.h>
+#include <mach/task_info.h>
+#import "NSNotificationCenter+AllObservers.h"
 #define ENABLE_DBG_LOG 1
 
 #if ENABLE_DBG_LOG
@@ -25,6 +30,8 @@
 @property(nonatomic, strong)UIWebView* wkview;
 @property(nonatomic, strong)UITableView* tabView;
 @property(nonatomic, assign)NSInteger frameLoadCount;
+@property(nonatomic, strong)UILabel* infoLabel;
+@property(nonatomic, strong)NSTimer* updateTimer;
 @end
 
 @implementation ViewController
@@ -86,6 +93,37 @@
     btn.centerY = self.view.centerY;
     btn.backgroundColor = [[UIColor brownColor] colorWithAlphaComponent:0.5];
     [self.view addSubview:btn];
+
+    btn = [self buttonWithTitle:@"F"];
+    btn.tag = 4;
+    btn.left = self.view.left;
+    btn.bottom = self.view.bottom;
+    btn.backgroundColor = [[UIColor orangeColor] colorWithAlphaComponent:0.5];
+    [self.view addSubview:btn];
+    
+    btn = [self buttonWithTitle:@"B"];
+    btn.tag = 5;
+    btn.right = self.view.right;
+    btn.bottom = self.view.bottom;
+    btn.backgroundColor = [[UIColor orangeColor] colorWithAlphaComponent:0.5];
+    [self.view addSubview:btn];
+
+    btn = [self buttonWithTitle:@"T"];
+    btn.tag = 6;
+    btn.centerX = self.view.centerX;
+    btn.bottom = self.view.bottom;
+    btn.backgroundColor = [[UIColor cyanColor] colorWithAlphaComponent:0.5];
+    [self.view addSubview:btn];
+
+    
+    self.infoLabel = [[UILabel alloc] init];
+    self.infoLabel.width = self.view.width - btn.width * 3;
+    self.infoLabel.height = btn.height;
+    self.infoLabel.left = btn.width * 1.5;
+    self.infoLabel.textAlignment = NSTextAlignmentCenter;
+    self.infoLabel.font = [UIFont systemFontOfSize:14];
+    self.infoLabel.backgroundColor = [[UIColor grayColor] colorWithAlphaComponent:0.5];
+    [self.view addSubview:self.infoLabel];
 }
 
 - (CustomButton*)buttonWithTitle:(NSString*)title
@@ -111,11 +149,13 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [self registerForPreviewingWithDelegate:self sourceView:self.view];
+    self.updateTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateProperty) userInfo:nil repeats:YES];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
-
+    [self.updateTimer invalidate];
+    self.updateTimer = nil;
 }
 
 - (void)didReceiveMemoryWarning
@@ -175,7 +215,10 @@
     NSString* selName = [NSString stringWithFormat:@"onBtn_%ld", (long)idx];
     SEL sel = NSSelectorFromString(selName);
     
+    UIButton* btn = (UIButton*)sender;
+    btn.enabled = NO;
     ((void(*)(id,SEL))objc_msgSend)(self, sel);
+    btn.enabled = YES;
 }
 
 - (void)onBtn_0
@@ -202,6 +245,56 @@
     [_wkview goForward];
 }
 
+- (void)onBtn_4
+{
+    NSArray<NSString*>* notifications = @[
+                                          @"UIApplicationWillEnterForegroundNotification",
+                                          ];
+    
+    [notifications enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:obj object:nil];
+    }];
+}
+
+- (void)onBtn_5
+{
+    NSArray<NSString*>* notifications = @[
+                                          @"UIApplicationDidReceiveMemoryWarningNotification",
+                                          @"UIApplicationDidReceiveMemoryWarningNotification",
+                                          @"UIApplicationDidEnterBackgroundNotification"
+                                          ];
+    
+    @autoreleasepool {
+        [notifications enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:obj object:nil];
+        }];
+    }
+}
+
+
+
+- (void)onBtn_6
+{
+    NSSet* observers = [[NSNotificationCenter defaultCenter] observersForNotificationName:@"UIApplicationDidEnterBackgroundNotification"];
+    NSLog(@"Before: \n%@", observers);
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:[UIImage class]];
+    [observers enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
+       
+        if (
+//            [obj isKindOfClass:NSClassFromString(@"UIStatusBar")] ||
+//            [obj isKindOfClass:NSClassFromString(@"UIPeripheralHost")] ||
+//            [obj isKindOfClass:NSClassFromString(@"_UIAssetManager")] ||
+//            [obj isKindOfClass:NSClassFromString(@"_UIRemoteKeyboards")] ||
+//            [obj isKindOfClass:NSClassFromString(@"CASuspendNotification")] ||
+            0
+            )
+        {
+            [[NSNotificationCenter defaultCenter] removeObserver:obj];
+        }
+        
+    }];
+}
 #pragma mark- ButtonActions
 - (void)loadWithURLString:(NSString*)link
 {
@@ -242,6 +335,28 @@
 - (void)previewingContext:(id <UIViewControllerPreviewing>)previewingContext commitViewController:(UIViewController *)viewControllerToCommit NS_AVAILABLE_IOS(9_0)
 {
     [self.navigationController pushViewController:viewControllerToCommit animated:NO];
+}
+
+#pragma mark- memory useage
++ (double)curUsedMemoryVSize
+{
+    struct task_basic_info         info;
+    kern_return_t           rval = 0;
+    mach_port_t             task = mach_task_self();
+    mach_msg_type_number_t  tcnt = TASK_BASIC_INFO_COUNT;
+    task_info_t             tptr = (task_info_t) &info;
+    
+    memset(&info, 0, sizeof(info));
+    
+    rval = task_info(task, TASK_BASIC_INFO, tptr, &tcnt);
+    if (!(rval == KERN_SUCCESS)) return 0;
+    
+    return info.virtual_size/1024.0;
+}
+
+- (void)updateProperty
+{
+    self.infoLabel.text = [NSString stringWithFormat:@"VM Use : %.2f M", [[self class] curUsedMemoryVSize] / 1024.0];
 }
 
 @end
