392feeb6bc76ba13ba0e771d449b2d746103df3d
wangqianzhou <wangqianzhou@foxmail.com>
2017-04-07 11:24:13 +0800

    add web style weex

---
+++ BannerWeex/src/web.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <web id="webview" :src="src" class="webStyle"></web>
+    </div>
+</template>
+
+<style>
+  .webStyle
+  {
+    flex:1;
+    height: 700;
+    background-color: #8B0000;
+  }
+
+</style>
+
+<script>
+
+  var web_module = weex.requireModule('@weex-module/webview');
+
+  module.exports = {
+    data: {
+      src : "https://h5.m.taobao.com",
+    },
+
+    methods: {
+      onappear (event) {
+      }
+    }
+  }
+</script>
